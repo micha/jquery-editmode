a2aaf3da1e22ffb82d6e2b6eae5450118a674d3b
Micha Niskin <asdf@ubergibson.com>
2009-07-10 01:16:25 +0000

    asdf

--- jquery.editmode.js
+++ jquery.editmode.js
@@ -81,7 +81,6 @@
           $("style").remove();
           $("body").siblings().not($("head")).remove();
           var tb = $("#editmode").remove();
-          $("head").prepend(st);
           var html =  "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\"\n"+
                       "    \"http://www.w3.org/TR/html4/strict.dtd\">\n"+
                       "<html lang=\"en\">\n"+
